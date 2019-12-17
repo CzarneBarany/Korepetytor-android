6cc812664757232bdf3fe784d717a329aa13ea17
veraxys <veraxys@gmail.com>
2019-12-17 22:32:16 +0100

    logowanie fix

--- app/src/main/java/com/czarneBarany/korepetytorsi/Login.java
+++ app/src/main/java/com/czarneBarany/korepetytorsi/Login.java
@@ -2,8 +2,10 @@ package com.czarneBarany.korepetytorsi;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,33 +45,28 @@ public class Login extends AppCompatActivity {
                 try {
                     log.put("email", email.getText());
                     log.put("password", password.getText());
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
-               // login(log);
+                login(log);
 
-                /*try {
+                try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }*/
+                }
 
-                SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
-                preferences.edit().putString("accountId", "5").apply();
-                //preferences.edit().putString("jwtToken", response.getString("jwtToken")).apply();
-                //preferences.edit().putString("role", response.getString("role")).apply();
                 Intent intent = new Intent(getApplicationContext(), MainPage.class);
                 startActivity(intent);
-
-
 
             }
         });
 
-
-
     }
+
+
 
     private void login(JSONObject log) {
         VolleyLog.e(log.toString());
@@ -81,12 +78,13 @@ public class Login extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
-                        try {
+                        Log.d("JD",response.toString());
+
+                         try {
                             preferences.edit().putString("accountId", response.getString("accountId")).apply();
                             preferences.edit().putString("jwtToken", response.getString("jwtToken")).apply();
                             preferences.edit().putString("role", response.getString("role")).apply();
-                            Intent intent = new Intent(getApplicationContext(), MainPage.class);
-                            startActivity(intent);
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -100,12 +98,12 @@ public class Login extends AppCompatActivity {
             }
         })
         {
-            @Override
+            /*@Override
             public Map<String,String> getHeaders() throws AuthFailureError{
                 Map<String, String> params = new HashMap<>();
                 params.put("Authorization", "Bearer "+ getSharedPreferences("myPrefs", MODE_PRIVATE).getString("jwtToken",""));
                 return params;
-            }
+            }*/
         };
         queue.add(stringRequest);
     }
